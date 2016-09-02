ca29ee6cfeeab2b69fdb7cf46cd6734e05ec1727
Ralph Moser <ralph@ramosoft.ch>
2016-09-02 18:23:07 +0200

    - google events added for brochure download and consulting

--- all/themes/hso_theme/js/main.js
+++ all/themes/hso_theme/js/main.js
@@ -520,7 +520,104 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) 
     }
   };
 
-  $.fn.extend({
+  /**
+   * Implements google analytic events for the brochure download and the consulting request forms.
+   */
+  Drupal.behaviors.gaEvents = {
+    attach: function () {
+      //
+      // GA Events for the brochure download, consulting requests and brochure postal delivery
+      var $brochureForm = $('#webform-client-form-1336'),
+        //$brochurePostal = $('#webform-client-form-14006'),
+        $consultingForm = $('#webform-client-form-1335'),
+        _createFunctionWithTimeout = function (callback, opt_timeout) {
+          var called = false;
+          var fn = function () {
+            if (!called) {
+              called = true;
+              callback();
+            }
+          };
+          setTimeout(fn, opt_timeout || 1000);
+          return fn;
+        };
+
+      //
+      // Adds a listener for the brochure download.
+      $brochureForm.unbind('submit');
+      $brochureForm.bind('submit', function (event) {
+        // Prevents the browser from submitting the form
+        // and thus unloading the current page.
+        event.preventDefault();
+
+        // get form values
+        var segment = $(this).find('#webform-component-interesse select option:selected').html(),
+          course = $(this).find('#webform-component-lehrgang select option:selected').html(),
+          label = segment + ' - ' + course;
+
+        // Sends the event to Google Analytics and
+        // resubmits the form once the hit is done.
+        ga('send', 'event', 'Brochure-download-form', 'submit', label, {
+          hitCallback: _createFunctionWithTimeout(function () {
+            $brochureForm.unbind('submit');  // prevents infinite loop
+            $brochureForm.submit();
+          })
+        });
+      });
+
+      //
+      // Adds a listener for the brochure postal delivery.
+      /*
+      $brochurePostal.unbind('submit');
+      $brochurePostal.bind('submit', function (event) {
+        // Prevents the browser from submitting the form
+        // and thus unloading the current page.
+        event.preventDefault();
+
+        // get form values
+        var segment = $(this).find('#webform-component-interesse select option:selected').html(),
+          course = $(this).find('#webform-component-lehrgang select option:selected').html(),
+          stao = $(this).find('#webform-component-standort select option:selected').html(),
+          label =  stao + ' - ' + segment + ' - ' + course;
+
+        // Sends the event to Google Analytics and
+        // resubmits the form once the hit is done.
+        ga('send', 'event', 'Brochure-delivery-form', 'submit', label, {
+          hitCallback: _createFunctionWithTimeout(function () {
+            $brochurePostal.unbind('submit');  // prevents infinite loop
+            $brochurePostal.submit();
+          })
+        });
+      });
+*/
+
+      //
+      // Adds a listener for the consulting form.
+      $consultingForm.unbind('submit');
+      $consultingForm.bind('submit', function (event) {
+        // Prevents the browser from submitting the form
+        // and thus unloading the current page.
+        event.preventDefault();
+
+        // get form values
+        var label = $(this).find('#webform-component-interesse select option:selected').html();
+
+        // Sends the event to Google Analytics and
+        // resubmits the form once the hit is done.
+        ga('send', 'event', 'Consulting-form', 'submit', label, {
+          hitCallback: _createFunctionWithTimeout(function () {
+            $consultingForm.unbind('submit'); // prevents infinite loop
+            $consultingForm.submit();
+          })
+        });
+      });
+
+    }
+  };
+
+
+
+$.fn.extend({
     linkUrl: function () {
       var returning = [];
       var regexp    = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
